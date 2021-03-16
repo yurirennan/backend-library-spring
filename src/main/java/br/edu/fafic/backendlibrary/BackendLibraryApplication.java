1b903c756105bb138a386fad1a298e358ccc7fd0
Yuri Rennan <yurirennan14@gmail.com>
2021-03-15 21:10:05 -0300

    Initial commit

---
+++ src/main/java/br/edu/fafic/backendlibrary/BackendLibraryApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fafic.backendlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fafic/backendlibrary/BackendLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fafic.backendlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
